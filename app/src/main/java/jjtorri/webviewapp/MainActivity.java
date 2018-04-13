9033fd466448ab223c34d59604984adf7ef4c5b5
JJ Torrijos <33837794+jjtorrijosc@users.noreply.github.com>
2018-04-13 09:54:28 +0200

    configuration changes, back button control

--- app/src/main/java/jjtorri/webviewapp/MainActivity.java
+++ app/src/main/java/jjtorri/webviewapp/MainActivity.java
@@ -16,12 +16,25 @@ public class MainActivity extends AppCompatActivity {
         WebView mainWebView = (WebView) findViewById(R.id.main_webview);
         WebSettings webSettings = mainWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        ;
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.supportMultipleWindows();
+
         mainWebView.addJavascriptInterface(
                 new AndroidId(
                            Settings.Secure.getString(getApplicationContext().getContentResolver(),
                            Settings.Secure.ANDROID_ID)),
                        "androidId");
         mainWebView.loadUrl("file:///android_asset/test.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView mainWebView = (WebView) findViewById(R.id.main_webview);
+        if(mainWebView.canGoBack()) {
+            mainWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
